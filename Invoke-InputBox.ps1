8739e5567c98e99836c5905437f51d623bbd6050
McBoomerson <43789545+McBoomerson@users.noreply.github.com>
2018-10-02 11:26:27 -0500

    Adding first code snippets

---
+++ Invoke-InputBox.ps1
@@ -0,0 +1,2 @@
+﻿Invoke-expression "C:\Tools\PowerShell\InputBoxTesting.ps1 -ListLocation C:\Tools\PowerShell\inputSites.csv -CSV_column Server"
+Write-Host "You selected $ReturnValue"
---
+++ LocalAccountExpirationCheck.ps1
@@ -0,0 +1,9 @@
+﻿$Users = Get-LocalUser |  Where {($_.Enabled -eq $true) -and ($_.PasswordExpires -ne $null)}  | select Name, PasswordExpires
+$Today = Get-Date
+ 
+foreach ($user in $users) {
+    $date = [datetime]$user.PasswordExpires
+    $Name = $User.Name
+    $Expires = New-TimeSpan -Start $today -End $date
+    write-host `n "$Name expires in $Expires days" `n
+}
